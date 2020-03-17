c0a3b7d2f04fdecc1ccc4fa2eb47be7ef78f9290
Ondra Chaloupka <ochaloup@redhat.com>
2020-03-17 17:24:19 +0100

    adding priority to interceptor

--- src/main/java/cz/chalda/quarkus/cdi/MyInterceptor.java
+++ src/main/java/cz/chalda/quarkus/cdi/MyInterceptor.java
@@ -3,10 +3,12 @@ package cz.chalda.quarkus.cdi;
 import javax.interceptor.AroundInvoke;
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+import javax.annotation.Priority;
 
 @Interceptor
 @AnnotationWithInit
 @AnnotationSecond
+@Priority(Interceptor.Priority.PLATFORM_BEFORE + 200)
 public class MyInterceptor {
 
     @AroundInvoke
